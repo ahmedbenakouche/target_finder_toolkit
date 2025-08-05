d589f6af13145d931aab3596ee142ca002953389
AHMEDBENAKOUCHE <152164619+AHMEDBENAKOUCHE@users.noreply.github.com>
2025-08-05 18:49:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: MIT',
     ],
 )
+
 
